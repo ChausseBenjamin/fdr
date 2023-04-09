8d9fcdff3193e2494f454da94ae11913aa3f4172
Benjamin Chausse <benjamin@chausse.xyz>
2023-04-09 19:57:33 -0400

    Modular rootPath

--- fdr-mvp/main.cpp
+++ fdr-mvp/main.cpp
@@ -31,39 +31,31 @@ int main(int argc, char *argv[]) {
   // Mount the scene to the QGraphicsView
   view->setScene(scene);
 
-  QString songFiles[10] = {
-    "/home/master/Workspace/fdr/songs/Greta-Van-Fleet-Highway-Tune/notes.chart",
-    "/home/master/Workspace/fdr/songs/Joan-Jett-and-the-Blackhearts-I-Love-Rock-_N-Roll-(The-Arrows-Cover)/notes.chart",
-    "/home/master/Workspace/fdr/songs/Maynard-Ferguson-Birdland/notes.chart",
-    "/home/master/Workspace/fdr/songs/Maynard-Ferguson-Country-Road-(James-Taylor-Cover)/notes.chart",
-    "/home/master/Workspace/fdr/songs/Maynard-Ferguson-Theme-From-Shaft/notes.chart",
-    "/home/master/Workspace/fdr/songs/Owane-Rock-Is-Too-Heavy/notes.chart",
-    "/home/master/Workspace/fdr/songs/Santana-Oye-Como-Va-(Tito-Puente-Cover)/notes.chart",
-    "/home/master/Workspace/fdr/songs/Stevie-Wonder-Contusion/notes.chart",
-    "/home/master/Workspace/fdr/songs/Symphony-X-Eve-of-Seduction/notes.chart",
-    "/home/master/Workspace/fdr/songs/Victor-Wooten-and-Steve-Bailey-A-Chick-from-Corea-(Live)/notes.chart"
+  const QString sourcePath = "/home/master/Workspace/fdr";
+  const QString songPath = sourcePath+"/songs/";
+  QString songNames[10] = {
+    "Greta-Van-Fleet-Highway-Tune/notes.chart",
+    "Joan-Jett-and-the-Blackhearts-I-Love-Rock-_N-Roll-(The-Arrows-Cover)/notes.chart",
+    "Maynard-Ferguson-Birdland/notes.chart",
+    "Maynard-Ferguson-Country-Road-(James-Taylor-Cover)/notes.chart",
+    "Maynard-Ferguson-Theme-From-Shaft/notes.chart",
+    "Owane-Rock-Is-Too-Heavy/notes.chart",
+    "Santana-Oye-Como-Va-(Tito-Puente-Cover)/notes.chart",
+    "Stevie-Wonder-Contusion/notes.chart",
+    "Symphony-X-Eve-of-Seduction/notes.chart",
+    "Victor-Wooten-and-Steve-Bailey-A-Chick-from-Corea-(Live)/notes.chart"
   };
 
   Song* repertoire[10];
   for (int i=0;i<10;i++){
-    repertoire[i] = new Song(songFiles[i]);
+    repertoire[i] = new Song(songPath+songNames[i]);
     for (int j=0;j<4;j++){
     };
   };
-  int wantedDiff = DIFFICULTY_HARD;
-  int wantedSong = 0;
-  repertoire[wantedSong]->parseSync();
+  int wantedDiff = DIFFICULTY_EXPERT;
+  int wantedSong = 5;
   repertoire[wantedSong]->setScene(scene);
-  repertoire[wantedSong]->parseDifficulty(wantedDiff);
   repertoire[wantedSong]->play(wantedDiff);
-
-  // int wantedDiff = DIFFICULTY_EASY;
-  // Song* testSong = new Song(songFiles[5]);
-  // testSong->parseSync();
-  // testSong->setScene(scene);
-  // testSong->parseDifficulty(wantedDiff);
-  // testSong->play(wantedDiff);
-
 
   // Display the viewport
   view->show();
